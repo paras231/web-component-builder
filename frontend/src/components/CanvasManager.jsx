4b6dd19b9a156569cddd9ffcedc8cd6d77a7bf26
paras231 <sharmaparas553@gmail.com>
2024-11-27 00:05:41 +0530

    updated generating dom tree

--- frontend/src/components/CanvasManager.jsx
+++ frontend/src/components/CanvasManager.jsx
@@ -5,13 +5,33 @@ const CanvasManager = (props) => {
     id: "droppable",
   });
 
+
   return (
     <section
       ref={setNodeRef}
       style={{ border: "1px solid whitesmoke", opacity: isOver ? 0.7 : 1 }}
       className="h-[30vmax] bg-white w-[60vmax] rounded-md mb-20"
-    ></section>
+    >
+      {props.canvasContent?.map((node, index) => renderDomTree(node))}
+    </section>
   );
 };
 
 export default CanvasManager;
+
+const renderDomTree = (node) => {
+  const { tagName, props = {}, children = [] } = node;
+  const Tag = tagName;
+  
+  return (
+    <Tag {...props}>
+      {Array.isArray(children)
+        ? children.map((child, index) =>
+            typeof child === "string" || typeof child === "number"
+              ? child
+              : renderDomTree(child)
+          )
+        : children}
+    </Tag>
+  );
+};
--- frontend/src/utils/ondropUtil.js
+++ frontend/src/utils/ondropUtil.js
@@ -10,24 +10,23 @@ export function handleElementAfterDrop(active, over, data, ...args) {
 
   // first we need to check if there is any element on canvas and we are dropping more elements on it , this basically creates a concept of DOM tree
   // we need to keep track on positions of elements which are in dom tree and which are being dropped
-  // 
+  //
   // check if there is any element on canvas
-  if(canvasContent && canvasContent.length > 0){
-
+  if (canvasContent && canvasContent.length > 0) {
   }
   if (active && active.data.current.tagName === "div") {
-    const container = data.collisions[0].data;
-    container.droppableContainer.node.innerHTML = `<div className=""></div>`;
     // console.log(container.droppableContainer.node);
     setCanvasContent([
       ...canvasContent,
       {
         tagName: "div",
-        innerHTML: `<div className=""></div>`,
+        props: { className: "flex justify-center items-center " },
+        children: [
+          { tagName: "h1", children: "Here is h1 tag" },
+          "Some additional text here",
+        ],
       },
     ]);
   } else if (active && active.data.current.tagName === "form") {
-    const container = data.collisions[0].data;
-    container.droppableContainer.node.innerHTML = `<form className=""></form>`;
   }
 }
